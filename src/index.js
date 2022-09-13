e00803c596e9db933db27f3f31d5b89cd297387c
Konrad <Synowiec400@wp.pl>
2022-09-13 15:02:12 +0200

    Added script for showing/hiding signup menu
    Added a simple script for showing and hiding signup menu on click and when clicking outside the component

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />} />
+      <Route exact path='/' element={<App />} />
       <Route path='/recipe-list' element={<RecipeListPage />} />
     </Routes>
   </BrowserRouter>
